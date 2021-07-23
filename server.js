9afa41be85ce596e310f5046964508f6c732e707
Jaime Salgado <jsalg10@gmail.com>
2021-07-23 12:30:54 -0500

    creating server without express

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 const http = require("http");
+const fs = require("fs");
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  res.setHeader("content-type", "text/html");
+  let route = "./views/";
+
+  switch (req.url) {
+    case "/":
+      route += "index.html";
+      break;
+    case "/contact":
+      route += "contact.html";
+      break;
+
+    default:
+      route += "404.html";
+      break;
+  }
+
+  fs.readFile(route, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, () => {
