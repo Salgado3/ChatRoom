06f07c5cd8cd5bce58251c9e1c7552b5ea80cdaf
Jaime Salgado <jsalg10@gmail.com>
2021-07-24 18:37:45 -0500

    added materialized css and ejs

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("./views/index.html");
+  res.sendFile("./views/index.html", { root: __dirname });
 });
 
+app.get("/add-item.html", (req, res) => {
+  res.sendFile("./views/add-item.html", { root: __dirname });
+});
+app.set("view engine", "ejs");
 app.listen(3000);
+app.use((req, res) => {
+  res.sendFile("./views/error.html", { root: __dirname });
+});
