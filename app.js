61eddc83ed5d93a705fb3e0b20050829fd15ae1b
Jaime Salgado <jsalg10@gmail.com>
2021-09-04 23:14:59 -0500

    working on getting tasks to add to db, need to fix mongoose on app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //setting up dotenv
 require('dotenv').config({ path: "./config/.env" })
-
 const express = require("express");
 const mongoose = require("mongoose");
+const Item = require('./models/items');
 const app = express();
 const mongodb = process.env.MongoDB_Key;
 
@@ -12,11 +12,21 @@ mongoose.connect(mongodb)
 
 
 
-
+//setting express to run on ejs
 app.set("view engine", "ejs");
 app.listen(process.env.PORT || 8000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
+
+app.get('/create-item', (req,res)=>{
+  const Item = new Item({
+    name: "phone",
+    price: 2000
+  }) 
+  Item.save()
+  .then(result=> res.send(result))
+})
+
 
 
 app.get("/", (req, res) => {
--- models/items.js
+++ models/items.js
@@ -1,18 +1,15 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
     name: {
-        type:string,
-        required:true
+        type: String,
+        required: true
     },
     price:{
-        type:number,
+        type: Number,
         required:true
     }
 },{timestamps:true})
-
-
 const Item = mongoose.model("Item", itemSchema)
-
 module.exports = Item;
